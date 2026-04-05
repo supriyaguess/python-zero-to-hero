2010b3aef1af3fb7eae79a39a1186e1a0b9b3702
Sanoj-Omli <kumarisupriyasoni96@gmail.com>
2026-04-05 16:43:02 +0530

    list and tuple

---
+++ listandtuples/list.py
@@ -0,0 +1,44 @@
+# student = ["Supriya", 95.4,17,"Delhi"]
+# print(student[0])
+# student[0] = "Suraj"
+# print(student) 
+
+#slicing
+# marks = [85,94,76,63,48]
+# print(marks[-3:-1])
+# print(type(student))
+
+# marks.append(87)
+# marks.sort()
+# print(marks)
+
+# tup = ()
+# print(tup)
+# print(type(tup))
+
+# str = "Hello"
+# print(str[0])
+# str[0] = "y" #'str' object does not support item assignment 
+# movies = []
+# mov1 = input("enter 1st movies : ")
+# movies.append(mov1)
+
+# mov2 = input("enter 2nd movies : ")
+# movies.append(mov2)
+
+# mov3 = input("enter 3rd movies : ")
+# movies.append(mov3)
+
+# print(movies)
+
+list1 = [1,2,1]
+list2 = [1,2,3]
+
+copy_list1 = list1.copy()
+copy_list1.reverse()
+
+if(copy_list1 == list1):
+    print("palindrome")
+else:
+    print("NOT palidrome")
+
