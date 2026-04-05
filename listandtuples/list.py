24f7cf32e0e07243f3719da304bfe4b3663847a7
Sanoj-Omli <kumarisupriyasoni96@gmail.com>
2026-04-05 16:47:28 +0530

    Update list.py

--- listandtuples/list.py
+++ listandtuples/list.py
@@ -28,17 +28,23 @@
 
 # mov3 = input("enter 3rd movies : ")
 # movies.append(mov3)
-
+ 
 # print(movies)
 
-list1 = [1,2,1]
-list2 = [1,2,3]
+# list1 = [1,2,1]
+# list2 = [1,2,3]
 
-copy_list1 = list1.copy()
-copy_list1.reverse()
+# copy_list1 = list1.copy()
+# copy_list1.reverse()
 
-if(copy_list1 == list1):
-    print("palindrome")
-else:
-    print("NOT palidrome")
+# if(copy_list1 == list1):
+#     print("palindrome")
+# else:
+#     print("NOT palidrome")
+# grade = ("C", "D","A","A","B","B","A")
+# print(grade.count("A"))
+grade = ["C", "D","A","A","B","B","A"]
+grade.sort()
+print(grade)
+
 
